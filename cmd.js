e8dc078702b3c6df0e66ae65bb13abd268e18515
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2017-09-08 19:51:35 +0900

    Pass tests

--- cmd.js
+++ cmd.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
-var fs = require('fs')
 var path = require('path')
 var pythonScriptFilePath = path.resolve(__dirname, './actions-to-json.py')
+// Write the filename of the python script so stdout so that it can be used in a bash command that invokes blender
+// i.e.
+//  blender /path/to/blender/file.blend --python `actions-to-json` -- /path/to/my/output/file.json
+//
 console.log(pythonScriptFilePath)
