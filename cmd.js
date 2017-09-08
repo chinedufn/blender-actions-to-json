e23e894f74914584b5851e4001fae5b55f9a32f7
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2017-09-08 19:44:55 +0900

    Set up binary command

---
+++ cmd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+var fs = require('fs')
+var path = require('path')
+var pythonScriptFilePath = path.resolve(__dirname, './actions-to-json.py')
+console.log(pythonScriptFilePath)
